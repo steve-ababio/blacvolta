515505ab3fa7afdd781a1725a8d621fb069ef1ca
steve-ababio <stevo12424@live.com>
2024-04-05 12:32:01 +0000

    changed the background colour and text color of the calender page

--- app/calender/page.tsx
+++ app/calender/page.tsx
@@ -15,11 +15,11 @@ export default function Calender(){
         setDialogVisible(false);
     }
     return(
-        <div className="h-dvh">
+        <div className="h-dvh bg-blak">
             <Header />
             <div className="h-full flex flex-col items-center justify-center">
                 <div className="text-center -mt-[150px] mb-[90px]">
-                    <h1 className='poppins text-[20px] sm:text-[30px]'>UPCOMING EVENTS</h1>
+                    <h1 className='poppins text-[20px] sm:text-[30px] text-white'>UPCOMING EVENTS</h1>
                 </div>
                 <Calendar value={date}  onChange={(e) => {setDate(e.value);showEventDialog()}} inline/>
             </div>

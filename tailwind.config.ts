d2adfe85d376b5bfa7d5430ddf6be5432519291f
steve-ababio <stevo12424@live.com>
2024-04-05 12:19:50 +0000

    replace bg.jpg with bg.webp in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
         "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        "image":"linear-gradient(to bottom, rgba(7, 7, 7, 0.84),rgba(9, 9, 9, 0.73)),url(/assets/images/bg.jpg)",
+        "image":"linear-gradient(to bottom, rgba(7, 7, 7, 0.84),rgba(9, 9, 9, 0.73)),url(/assets/images/bg.webp)",
       },
     },
   },

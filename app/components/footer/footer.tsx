bd1c7011a830c00c9faabe0b4c652008e678134d
steve-ababio <stevo12424@live.com>
2024-04-05 12:30:23 +0000

    changed the background colour of the footer

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Footer(){
     return(
         <footer>
-            <div className="px-[5%] bg-white">
+            <div className="px-[5%] bg-black">
                 <div className=" border-t border-[#707070] py-[5%]">
                     <div className="flex flex-col gap-y-3 items-center sm:max-w-[30%]">
                         <div className="">

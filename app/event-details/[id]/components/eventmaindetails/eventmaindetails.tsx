31a838701280c22c7667fca12719dc530542dc42
steve-ababio <stevo12424@live.com>
2024-05-07 04:12:10 +0000

    made some changes

--- app/event-details/[id]/components/eventmaindetails/eventmaindetails.tsx
+++ app/event-details/[id]/components/eventmaindetails/eventmaindetails.tsx
@@ -8,7 +8,7 @@ interface EventMainDetailsProps extends React.HtmlHTMLAttributes<HTMLDivElement>
 }
 export default function EventMainInfo({Icon,title,titlesibling,className}:EventMainDetailsProps){
     return(
-        <div className="flex items-center gap-x-7 mb-6">
+        <div className="flex items-center gap-x-7 mb-7">
             <div className={twMerge(`flex justify-center items-center h-[42px] w-[42px] rounded-[8px]`,className)}>
                 <Icon className="text-slate-600" size={20} />
             </div>

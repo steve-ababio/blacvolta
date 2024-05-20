093c23cd9b14f8dee607bfcd248a71354e62c32a
steve-ababio <stevo12424@live.com>
2024-05-20 03:24:01 +0000

    made changes

--- app/event-details/[id]/components/eventmaindetails/eventmaindetails.tsx
+++ app/event-details/[id]/components/eventmaindetails/eventmaindetails.tsx
@@ -12,7 +12,7 @@ export default function EventMainInfo({Icon,title,titlesibling,className}:EventM
             <div className={twMerge(`flex justify-center shadow-md items-center h-[42px] w-[42px] rounded-[8px]`,className)}>
                 <Icon className="text-white" size={30} />
             </div>
-            <div className="text-white flex-[7]">
+            <div className="text-white flex-[7] text-wrap break-all">
                 <h2 className="text-[16px] kamerik font-bold">{title}</h2>
                 {titlesibling}
             </div>

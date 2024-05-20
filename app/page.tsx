9e06f336eb3ff1a6f87c70acdac4a63b1e40e54b
steve-ababio <stevo12424@live.com>
2024-05-20 23:17:13 +0000

    made some changes and added some  features

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,12 @@ import Home from "./components/home/home";
 import Footer from "./components/footer/footer";
 import CalenderModal from "./components/calendar/calendar";
 import BlogPosts from "./components/blogposts/blogposts";
+import { unstable_noStore } from "next/cache";
 
-export default async function HomePage() {
-  
+export const dynamic = 'force-dynamic';
+export default async function HomePage() 
+{
+  unstable_noStore();  
   return (
     <main className="min-h-screen">
       <Home />

72269d26dfe1c3f38ab8f7494adcbe6248e3b28c
steve-ababio <stevo12424@live.com>
2024-05-20 23:03:14 +0000

    made some changes and added some  features

--- app/api/latestblogs/route.ts
+++ app/api/latestblogs/route.ts
@@ -1,15 +1,15 @@
-import { prisma } from "@/app/lib/prisma";
-import { NextResponse } from "next/server";
+// import { prisma } from "@/app/lib/prisma";
+// import { NextResponse } from "next/server";
 
-async function getBlogLatestPosts(){
-    return await prisma.blogPost.findMany({
-        orderBy:{
-            id:"desc"
-        },
-        take:3
-    });
-}
-export async function GET(){
-    const latestblogs = await getBlogLatestPosts();
-    return NextResponse.json(latestblogs);
-}
+// async function getBlogLatestPosts(){
+//     return await prisma.blogPost.findMany({
+//         orderBy:{
+//             id:"desc"
+//         },
+//         take:3
+//     });
+// }
+// export async function GET(){
+//     const latestblogs = await getBlogLatestPosts();
+//     return NextResponse.json(latestblogs);
+// }

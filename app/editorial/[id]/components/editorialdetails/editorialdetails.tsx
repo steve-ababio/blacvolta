81bf034f6bc31027175c30a10d3364eb853bb9af
steve-ababio <stevo12424@live.com>
2024-05-24 04:08:35 +0000

    made changes

--- app/editorial/[id]/components/editorialdetails/editorialdetails.tsx
+++ app/editorial/[id]/components/editorialdetails/editorialdetails.tsx
@@ -12,7 +12,7 @@ export default function EditorialDetails({title,author,blogdate,blogimagepath}: 
     return(
         <div className="border-b w-full pb-3 mt-6 border-b-slate-300/40">
             <h1 className="text-[35px] md:text-[40px] mb-5 kamerik font-black md:text-center text-white">{title}</h1>
-            <img src={blogimagepath} className="max-w-full max-h-full mb-6 text-white" alt="paragraph image" />
+            <img src={blogimagepath} className="max-w-full max-h-full w-full mb-6 text-white" alt="paragraph image" />
             <div className="flex items-center mb-2 gap-x-3">
                 <FaRegUser className="text-white" size={20} />
                 <h2 className="text-white md:text-[18px] text-[16px] font-bold text-slate-white">{author}</h2>

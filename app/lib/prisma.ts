061c5be047d93a2b3f2634db56e0096243eaa356
steve-ababio <stevo12424@live.com>
2024-09-12 11:20:17 +0000

    changed prisma code

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,7 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-declare global {
-    var prisma:PrismaClient | undefined
-}
-export const prisma = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+

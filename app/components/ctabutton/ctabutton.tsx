5aa3f1c0061623c80caf0a2be40fd77a8eb2d38a
steve-ababio <stevo12424@live.com>
2025-03-05 17:56:19 +0000

    reduce publish event button size

--- app/components/ctabutton/ctabutton.tsx
+++ app/components/ctabutton/ctabutton.tsx
@@ -9,8 +9,8 @@ interface CtaButtonProps extends React.HtmlHTMLAttributes<HTMLButtonElement>{
 export default function CtaButton({href,label,className}:CtaButtonProps) {
     return(
         <Link className={twMerge(`
-            block w-[90%] max-w-[15rem] text-center h-full
-          bg-white text-[0.88rem] py-3 px-4
+            block w-[90%] max-w-[12rem]  sm:max-w-[15rem] text-center h-full
+          bg-white sm:text-[0.88rem] py-3 px-4 text-[12px]
           text-slate-700 rounded-[40px] font-bold focus-visible:outline-2
          focus:outline-white focus:outline-offset-[4px]
             duration-300 hover:bg-[#bdbdbd]`,className)} href={href}>

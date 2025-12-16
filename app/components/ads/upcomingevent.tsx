ab226e39167d8e974723cd8d843295bc5770828c
steve-ababio <stevo12424@live.com>
2025-12-16 15:23:41 +0000

    feat: add visa banners

--- app/components/ads/upcomingevent.tsx
+++ app/components/ads/upcomingevent.tsx
@@ -2,11 +2,14 @@ import Image from "next/image";
 
 export default function Ads(){
     return(
-        <section className="pt-[3%] bg-black">
-            <div>
+        <section className="mt-12 bg-black">
+            <div className="flex justify-center flex-col items-center md:flex-row md:gap-0 gap-6">
                 {/* <video className="mx-auto block w-full sm:w-fit sm:rounded-[10px]" src="/assets/videos/Blacvolta.mp4" autoPlay muted loop playsInline></video> */}
-                <div>
-                    <img src="/assets/images/ads/mukuru.webp"  className="mx-auto w-[90%] max-w-[800px] height-full" alt="mukuru ad" />
+                <div className="flex items-center justify-center">
+                    <img src="/assets/images/ads/Web_Banner_Melcom.png"  className="w-[90%] max-w-[800px] height-full" alt="mukuru ad" />
+                </div>
+                <div className="flex items-center justify-center">
+                    <img src="/assets/images/ads/Web_Banner_Pizzaman_Chickenman.png"  className="w-[90%] max-w-[800px] height-full" alt="mukuru ad" />
                 </div>
             </div>
         </section>

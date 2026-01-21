e17edc69e8107e1464d6075c5db55897cacfd799
steve-ababio <stevo12424@live.com>
2026-01-21 11:10:20 +0000

    Add deep linking verification files

--- app/event-details/[id]/components/eventdetailsfooter/eventdetailsfooter.tsx
+++ app/event-details/[id]/components/eventdetailsfooter/eventdetailsfooter.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import EventDetailFooterItem from "@/app/event-details/[id]/components/eventdetailsfooteritem/eventdetailsfooteritem";
 import { BsDownload,BsEnvelopePlus,BsCalendar4 } from "react-icons/bs";
 import { addToCalender, downloadFile } from "@/app/utils/utils";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { RotatingLines } from "react-loader-spinner";
 import { MdManageSearch } from "react-icons/md";
 
@@ -28,18 +28,96 @@ export default function EventDetailFooter({description,flyerImagePath,sociallink
         downloadFile(imageurl,"eventflyer");
         setDownloading(false);
     }
-    return(
-        <div>
+    const [open, setOpen] = useState(false);
+
+
+    // Prevent background scroll when sheet is open
+    useEffect(() => {
+    if (open) {
+    document.body.style.overflow = "hidden";
+    } else {
+    document.body.style.overflow = "";
+    }
+    return () => {
+    document.body.style.overflow = "";
+    };
+    }, [open]);
+    
+    
+    return (
+        <>
             <div className="px-[10px]">
-                {
-                    description.length > 0 &&
-                    <>
-                        <h2 className="font-kamerik pt-[20px] pb-[5px] text-slate-200 font-bold text-[20px]">About this event </h2>
-                        <p className="text-[1rem] text-[#9A9A9A]">{description}</p>
-                    </>
-                }
+            {
+                description.length > 0 &&
+                <>
+                    <h2 className="font-kamerik pt-[20px] pb-[5px] text-slate-200 font-bold text-[20px]">About this event </h2>
+                    <p className="text-[1rem] text-[#9A9A9A]">{description}</p>
+                </>
+            }
             </div>
-            <div className="py-[40px] px-[10px] flex gap-2 items-start text-slate-200">
+            <div className="block md:hidden">
+                <div className="mt-4">
+                    <button
+                        onClick={() => setOpen(true)}
+                        className="px-6 py-3 font-semibold rounded-md bg-white text-black w-full shadow-2xl hover:opacity-90 transition"
+                    >
+                        Show More
+                    </button>
+                </div>
+
+                {/* Backdrop */}
+                {open && (
+                <div
+                onClick={() => setOpen(false)}
+                className="fixed inset-0 bg-black/40 z-40"
+                />
+                )}
+
+
+                {/* Bottom Sheet */}
+                <div
+                className={`fixed bottom-0 left-0 right-0 z-50 bg-slate-700 rounded-t-2xl shadow-2xl transform transition-transform duration-300 ease-out
+                ${open ? "translate-y-0" : "translate-y-full"}`}
+                style={{ height: "65vh" }}
+                >
+                {/* Handle */}
+                <div className="flex justify-center py-2">
+                <div className="h-1.5 w-12 rounded-full bg-gray-300" />
+                </div>
+
+                <div className="p-6">
+                <Link className="text-center flex-1" href={`tel:${phonenumber}`}><EventDetailFooterItem text="Inquiry" icon={<MdManageSearch size={25}/>}/></Link>
+                <EventDetailFooterItem  text="Add to calendar" onclick={(e)=>addToCalender(e,{eventDate,eventName,eventTime,venue,description})} icon={<BsCalendar4 size={25}/>}/>
+                <Link href={sociallink} className="text-center flex-1"><EventDetailFooterItem text="Follow Event" icon={<BsEnvelopePlus size={25}/>}/></Link>
+                {
+                    downloading ?
+                    <div className="flex flex-1 justify-center self-center">
+                        <RotatingLines 
+                            strokeColor="white" 
+                            strokeWidth="4"
+                            animationDuration="0.8"
+                            width="25"
+                            visible={true}
+                        />
+                    </div>
+                    :<EventDetailFooterItem 
+                        text="Download Flyer" 
+                        onclick={e=>downloadEventFlyer(e,flyerImagePath)} 
+                        icon={<BsDownload size={25}/>}
+                    />
+                }
+
+                <button
+                onClick={() => setOpen(false)}
+                className="w-full py-3 rounded-xl bg-gray-900 text-white"
+                >
+                Close
+                </button>
+                </div>
+                </div>
+            </div>
+            <div className="hidden md:block">
+                <div className="py-[40px] px-[10px] flex gap-2 items-start text-slate-200">
                 <Link className="text-center flex-1" href={`tel:${phonenumber}`}><EventDetailFooterItem text="Inquiry" icon={<MdManageSearch size={25}/>}/></Link>
                 <EventDetailFooterItem  text="Add to calendar" onclick={(e)=>addToCalender(e,{eventDate,eventName,eventTime,venue,description})} icon={<BsCalendar4 size={25}/>}/>
                 <Link href={sociallink} className="text-center flex-1"><EventDetailFooterItem text="Follow Event" icon={<BsEnvelopePlus size={25}/>}/></Link>
@@ -62,5 +140,41 @@ export default function EventDetailFooter({description,flyerImagePath,sociallink
                 }
             </div>
         </div>
+    </>
+    // return(
+    //     <div>
+    //         <div className="px-[10px]">
+    //             {
+    //                 description.length > 0 &&
+    //                 <>
+    //                     <h2 className="font-kamerik pt-[20px] pb-[5px] text-slate-200 font-bold text-[20px]">About this event </h2>
+    //                     <p className="text-[1rem] text-[#9A9A9A]">{description}</p>
+    //                 </>
+    //             }
+    //         </div>
+    //         <div className="py-[40px] px-[10px] flex gap-2 items-start text-slate-200">
+    //             <Link className="text-center flex-1" href={`tel:${phonenumber}`}><EventDetailFooterItem text="Inquiry" icon={<MdManageSearch size={25}/>}/></Link>
+    //             <EventDetailFooterItem  text="Add to calendar" onclick={(e)=>addToCalender(e,{eventDate,eventName,eventTime,venue,description})} icon={<BsCalendar4 size={25}/>}/>
+    //             <Link href={sociallink} className="text-center flex-1"><EventDetailFooterItem text="Follow Event" icon={<BsEnvelopePlus size={25}/>}/></Link>
+    //             {
+    //                 downloading ?
+    //                 <div className="flex flex-1 justify-center self-center">
+    //                     <RotatingLines 
+    //                         strokeColor="white" 
+    //                         strokeWidth="4"
+    //                         animationDuration="0.8"
+    //                         width="25"
+    //                         visible={true}
+    //                     />
+    //                 </div>
+    //                 :<EventDetailFooterItem 
+    //                     text="Download Flyer" 
+    //                     onclick={e=>downloadEventFlyer(e,flyerImagePath)} 
+    //                     icon={<BsDownload size={25}/>}
+    //                 />
+    //             }
+    //         </div>
+    //     </div>
     )
+
 }
--- app/event-details/[id]/components/eventdetailsfooteritem/eventdetailsfooteritem.tsx
+++ app/event-details/[id]/components/eventdetailsfooteritem/eventdetailsfooteritem.tsx
@@ -8,7 +8,7 @@ type EventDetailFooterItemProps = {
 export default function EventDetailFooterItem({icon,classname,text,onclick}:EventDetailFooterItemProps){
     return(
         <button className={twMerge(`cursor-pointer flex-1`,classname)} onClick={onclick}>
-            <div className="flex flex-col  items-center hover:text-blue-400 duration-200">
+            <div className="flex flex-row gap-3 md:flex-col md:gap-0 items-center hover:text-blue-400 duration-200">
                 <div>
                     {icon}
                 </div>

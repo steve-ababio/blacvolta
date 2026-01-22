3610e4462675e13acb4c4ba1e59d616d3334c938
steve-ababio <stevo12424@live.com>
2026-01-22 15:11:53 +0000

    feat: redesign event details page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,15 +4,15 @@ export default function About(){
     return(
         <main className="min-h-screen">
             <Hero headertext="ABOUT US" />
-            <div className="text-white text-justify md:px-[10%] lg:px-[20%] px-8 text-[1rem] leading-8 md:text-[1.1rem] py-12">
-                <p className="[word-spacing:-2px]">
+            <div className="text-white md:px-[10%] lg:px-[20%] px-8 text-[1rem] leading-8 md:text-[1.1rem] py-12">
+                <p >
                     BlacVolta is a dynamic digital media platform transforming
                     the entertainment and tech landscape across Africa.
                     Founded in October 2022, BlacVolta operates from its
                     headquarters in Ghana with a presence in East Africa,
                     the USA, and the UK.
                 </p>
-                <p className="[word-spacing:-2px]">
+                <p>
                     We specialize in media, digital marketing, content creation, 
                     videography, and e-commerce, all with the goal of reshaping
                     how African nightlife, culture, and technology are perceived
@@ -20,7 +20,7 @@ export default function About(){
                     and potential, BlacVolta is committed to celebrating the continent&apos;s 
                     rich cultural diversity through innovative projects and strategic collaborations.
                 </p>
-                <p className="[word-spacing:-2px]">
+                <p >
                     BlacVolta&apos;s impact is felt through initiatives like the
                     BlacVolta Nightlife Podcast, featuring international guests, 
                     and our expansion into Uganda with the Creatives & Creator Community Mixer.

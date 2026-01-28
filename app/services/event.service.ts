762ab5d5cd384dda5d8ec8e0b233805c40e4fe7a
steve-ababio <stevo12424@live.com>
2026-01-28 13:22:44 +0000

    fix: fetch events from another source

--- app/services/event.service.ts
+++
@@ -1,9 +0,0 @@
-import axios from "../lib/axios";
-
-export const EventService = {
-    getEvents: async (startDate: string,endDate:string, page = 1, limit = 50) => {
-        const url = `/api/events?date_from=${startDate}&date_to=${endDate}&page=${page}&${limit}&status=published`;
-        const res = await axios.get(url);
-        return res.data
-    }
-}
--- app/services/verification.service.ts
+++
@@ -1,8 +0,0 @@
-import { TicketData } from "@/common/types/types";
-import axios from "../lib/axios";
-export const TicketVerificationService = {
-    verifyTicket: async (payload: TicketData) => {
-        const res = await axios.post('/api/tickets/check-in', payload);
-        return res.data;
-    }
-}
